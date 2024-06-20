b9260b47d42c26d6c5347953a3a3b930cb0652f4
Spooshe21 <spoorthipshetty1234@gmail.com>
2024-06-20 21:39:01 +0530

    Updated comments

--- Arrays/src/arrays/ArrayReverse.java
+++ Arrays/src/arrays/ArrayReverse.java
@@ -6,7 +6,8 @@
  *---------------------------------------------------------------------------
  *   Purpose:  ArrayReverse.java
  *   Project:  Array Reversal
- *   Platform: JavaSE-22.0.1
+ *   Platform: Cross-platform (Windows, macOS, Linux)
+ *   Compiler: JDK-22
  *   IDE:  	   Eclipse IDE for Enterprise Java and Web Developers (includes Incubating components)
  *	           Version: 2024-03 (4.31.0)
  *             Build id: 20240307-1437
@@ -21,34 +22,34 @@ import java.util.Arrays;
  */
 public class ArrayReverse {
 
-    /**
-     * This method reverses the elements of an array.
-     * 
-     * @param arr The array to be reversed.
-     */
-    public static void reverseArray(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-        while (start < end) {
-            // Swap elements at start and end indices
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            // Move to the next pair of elements
-            start++;
-            end--;
-        }
-    }
+	/**
+	 * This method reverses the elements of an array.
+	 * 
+	 * @param arr The array to be reversed.
+	 */
+	public static void reverseArray(int[] arr) {
+		int start = 0;
+		int end = arr.length - 1;
+		while (start < end) {
+			// Swap elements at start and end indices
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			// Move to the next pair of elements
+			start++;
+			end--;
+		}
+	}
 
-    /**
-     * The main method to test the reverseArray method.
-     * 
-     * @param args The command-line arguments (not used).
-     */
-    public static void main(String[] args) {
-        int[] array = { 1, 2, 3, 4, 5 };
-        System.out.println("Original array: " + Arrays.toString(array));
-        reverseArray(array);
-        System.out.println("Reversed array: " + Arrays.toString(array));
-    }
+	/**
+	 * The main method to test the reverseArray method.
+	 * 
+	 * @param args The command-line arguments (not used).
+	 */
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 3, 4, 5 };
+		System.out.println("Original array: " + Arrays.toString(array));
+		reverseArray(array);
+		System.out.println("Reversed array: " + Arrays.toString(array));
+	}
 }
